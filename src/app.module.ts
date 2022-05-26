a6fead64d539c10f1c8de6e2c084838afc36029a
tuyennx <xuantuyen.nguyen@vmodev.com>
2022-05-26 13:04:00 +0700

    add cat module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatModule } from './cat/cat.module';
 import configurationYaml from '../configs/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configurationYaml]
-   })
+   }),
+    CatModule
   ],
   controllers: [AppController],
   providers: [AppService],
